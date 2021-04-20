9fe97daf1163493ba1dbd3e53ae9a6a15f332804
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2021-04-20 16:21:19 +0700

    title scraped

--- scraper.js
+++ scraper.js
@@ -9,7 +9,14 @@ async function scrapeProduct(url) {
     const src = await el.getProperty('src')
     const srcTxt = await src.jsonValue()
 
-    console.log({srcTxt})
+    const [el2] = await page.$x('//*[@id="productTitle"]');
+    const txt = await el2.getProperty("textContent");
+    const rawTxt = await txt.jsonValue()
+    const finalTxt = rawTxt.replace(/\n/g, "");
+
+    console.log({ srcTxt, finalTxt });
+
+    browser.close()
 }
 
 scrapeProduct('https://www.amazon.com/Eloquent-JavaScript-3rd-Introduction-Programming/dp/1593279507/ref=sr_1_1?dchild=1&keywords=eloquent+javascript&qid=1618908948&sr=8-1');
